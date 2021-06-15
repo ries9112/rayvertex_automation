f6709f5ba26612520e2a4ff6f7fdb8d5c8d9b79f
ries9112 <ries9112@colorado.edu>
2021-06-15 09:28:59 -0600

    add second example

--- script.R
+++ script.R
@@ -21,7 +21,10 @@ dev.off()
 # ANY WAY TO USE rayvertex FOR GGPLOTS? CAN IT BE ADDED TO LONG TUTORIAL TO PART THAT DOESN'T UPDATE?
 
 ## TEST 2
+mat2 = material_list(diffuse="grey80", ambient="grey80", ambient_intensity = 0.2)
+# initialize image
 png("test_rayvertex_2.png")
+# build scene
 generate_cornell_mesh(ceiling=FALSE) %>% 
   add_shape(sphere_mesh(position=c(555,555,555)/2, radius=80, material=mat)) %>% 
   add_shape(segment_mesh(start=c(555/2,0,555/2),end=c(555/2,196,555/2), 
@@ -32,3 +35,6 @@ generate_cornell_mesh(ceiling=FALSE) %>%
 #> Setting default values for Cornell box: lookfrom `c(278,278,-800)` lookat `c(278,278,0)` fov `40` .
 # Save image
 dev.off()
+
+
+
